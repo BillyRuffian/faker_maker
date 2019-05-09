baf69ed6a8b99704ba74865b25c91a94ae8c2cbc
codefactor-io <support@codefactor.io>
2019-05-09 16:15:15 +0000

    [CodeFactor] Apply fixes

--- spec/faker_maker/attribute_spec.rb
+++ spec/faker_maker/attribute_spec.rb
@@ -1,35 +1,33 @@
 RSpec.describe FakerMaker::Attribute do
-
   it 'has a name' do
     attr = FakerMaker::Attribute.new( :my_name, nil, {} )
     expect( attr.name ).to eq :my_name
   end
 
   it 'has a block' do
-    attr = FakerMaker::Attribute.new( :my_name, Proc.new{ 'block' } )
+    attr = FakerMaker::Attribute.new( :my_name, proc { 'block' } )
     expect( attr.block ).to be_a Proc
   end
 
   it 'can have a JSON alias' do
-    attr = FakerMaker::Attribute.new( :my_name, Proc.new{ 'block' }, {json: 'jsonString'} )
+    attr = FakerMaker::Attribute.new( :my_name, proc { 'block' }, json: 'jsonString' )
     expect( attr.translation? ).to be true
     expect( attr.translation ).to eq 'jsonString'
   end
 
   it 'allows cardinality > 1' do
-    attr = FakerMaker::Attribute.new( :my_name, Proc.new{ 'block' }, {has: 10} )
+    attr = FakerMaker::Attribute.new( :my_name, proc { 'block' }, has: 10 )
     expect( attr.cardinality ).to be 10
     expect( attr.array? ).to be true
   end
 
   it 'allow a range of cardinality' do
-    attr = FakerMaker::Attribute.new( :my_name, Proc.new{ 'block' }, {has: 1..5} )
+    attr = FakerMaker::Attribute.new( :my_name, proc { 'block' }, has: 1..5 )
     expect( attr.cardinality ).to be_between( 1, 5 )
   end
 
   it 'allows arrays for cardinality of 1' do
-    attr = FakerMaker::Attribute.new( :my_name, nil, {array: true} )
+    attr = FakerMaker::Attribute.new( :my_name, nil, array: true )
     expect( attr.array? ).to be true
   end
-
 end
